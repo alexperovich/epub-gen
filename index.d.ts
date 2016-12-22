8900c2b5c883076f35899dc379d1711c3d98fc7a
Alex Perovich <perovich.alex@outlook.com>
2016-12-21 18:14:34 -0800

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+declare namespace Epub
+{
+    interface Chapter
+    {
+        title?: string;
+        author?: string;
+        data: string;
+        excludeFromToc?: boolean;
+        beforeToc?: boolean;
+        filename?: string;
+    }
+    
+    interface Options
+    {
+        title: string;
+        author: string | string[];
+        publisher?: string;
+        cover?: string;
+        output?: string;
+        version?: 2 | 3;
+        css?: "string";
+        fonts?: string[];
+        lang?: "en" | string;
+        appendChapterTitles?: boolean;
+        customOpfTemplatePath?: string;
+        customNcxTocTemplatePath?: string;
+        customHtmlTocTemplatePath?: string;
+        content?: Chapter[];
+    }
+}
+
+declare class Epub
+{
+    constructor(options: Epub.Options, output?: string);
+    readonly promise: Promise<void>;
+}
+
+export = Epub;
